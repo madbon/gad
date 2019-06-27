cb465bc0872a7f9b95df1587deac409b4a600edc
Mark Bon <mdbon@dilg.gov.ph>
2019-06-28 07:51:59 +0800

    change label row id

--- common/modules/report/views/gad-plan-budget/_form.php
+++ common/modules/report/views/gad-plan-budget/_form.php
@@ -14,7 +14,7 @@ use kartik\select2\Select2;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'id')->textInput() ?>
+    <?= $form->field($model, 'id')->textInput()->label("Row Id") ?>
 
     <?= \file\components\AttachmentsInput::widget([
         'id' => 'file-input', // Optional
