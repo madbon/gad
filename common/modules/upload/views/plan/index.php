42f43d18a8d528280fe6e6b0b3a85ae52779b360
mdbon <mdbon@dilg.gov.ph>
2019-08-06 22:33:43 +0800

    dont show data preview if no excel data

--- common/modules/upload/views/plan/index.php
+++ common/modules/upload/views/plan/index.php
@@ -19,49 +19,48 @@ table.data_preview thead tr th
 	</div>
 </div>
 
-<div class="panel panel-warning">
-	<div class="panel-heading">
-		Data Preview
-		<?php
-			echo Html::a('<span class="glyphicon glyphicon-save"></span> Save', 
-				[
-					'save-excel-data'
-				], 
-           		[
-           			'class' => 'btn btn-sm btn-success pull-right', 
-           			'style' => 'margin-top:-5px;',
-           			'data' => ['confirm' => 'Are you sure you want to save this data?','method' => 'post']
-	         	]);
-		?>
-	</div>
-	<div class="panel-body">
-		<table class="table table-responsive table-bordered data_preview">
-			<thead>
-				<tr>
-					<th style="border-bottom: none;">Gender Issue or GAD Mandate</th>
-					<th style="border-bottom: none;">GAD Objective</th>
-					<th style="border-bottom: none;">Relevant LGU Program or Project</th>
-					<th style="border-bottom: none;">GAD Activity</th>
-					<th style="border-bottom: none;">Performance Indicator and Target</th>
-					<th colspan="3">GAD Budget</th>
-					<th style="border-bottom: none;">Lead or Responsible Office</th>
-				</tr>
-				<tr>
-					<th style="border-top: none;"></th>
-					<th style="border-top: none;"></th>
-					<th style="border-top: none;"></th>
-					<th style="border-top: none;"></th>
-					<th style="border-top: none;"></th>
-					<th>MOOE</th>
-					<th>PS</th>
-					<th>CO</th>
-					<th style="border-top: none;"></th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php
-					if(!empty($excelData))
-					{
+<?php if(!empty($excelData)){ ?>
+	<div class="panel panel-warning">
+		<div class="panel-heading">
+			Data Preview
+			<?php
+				echo Html::a('<span class="glyphicon glyphicon-save"></span> Save', 
+					[
+						'save-excel-data'
+					], 
+	           		[
+	           			'class' => 'btn btn-sm btn-success pull-right', 
+	           			'style' => 'margin-top:-5px;',
+	           			'data' => ['confirm' => 'Are you sure you want to save this data?','method' => 'post']
+		         	]);
+			?>
+		</div>
+		<div class="panel-body">
+			<table class="table table-responsive table-bordered data_preview">
+				<thead>
+					<tr>
+						<th style="border-bottom: none;">Gender Issue or GAD Mandate</th>
+						<th style="border-bottom: none;">GAD Objective</th>
+						<th style="border-bottom: none;">Relevant LGU Program or Project</th>
+						<th style="border-bottom: none;">GAD Activity</th>
+						<th style="border-bottom: none;">Performance Indicator and Target</th>
+						<th colspan="3">GAD Budget</th>
+						<th style="border-bottom: none;">Lead or Responsible Office</th>
+					</tr>
+					<tr>
+						<th style="border-top: none;"></th>
+						<th style="border-top: none;"></th>
+						<th style="border-top: none;"></th>
+						<th style="border-top: none;"></th>
+						<th style="border-top: none;"></th>
+						<th>MOOE</th>
+						<th>PS</th>
+						<th>CO</th>
+						<th style="border-top: none;"></th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
 						foreach ($excelData as $row) {
 							echo '<tr>';
 						  	echo 	'<td>' . $row[0] . '</td>';
@@ -75,12 +74,12 @@ table.data_preview thead tr th
 						  	echo 	'<td>' . $row[8] . '</td>';
 						  	echo '</tr>';
 						}
-					}
-				?>
-			</tbody>
-		</table>
+					?>
+				</tbody>
+			</table>
+		</div>
 	</div>
-</div>
+<?php } ?>
 <?php 
 // Html::a('<span class="glyphicon glyphicon-remove"></span> Cancel', ['index'], 
           // ['class' => 'btn btn-sm btn-danger pull-left',
