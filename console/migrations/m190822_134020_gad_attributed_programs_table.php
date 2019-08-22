de4aa42a71578d845cd1d1f713b33e0d821a5bf0
Mark Bon <bonmarkangelo@gmail.com>
2019-08-22 21:46:10 +0800

    layout login / migration for attributed_program

---
+++ console/migrations/m190822_134020_gad_attributed_programs_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190822_134020_gad_attributed_programs_table
+ */
+class m190822_134020_gad_attributed_programs_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m190822_134020_gad_attributed_programs_table cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m190822_134020_gad_attributed_programs_table cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
