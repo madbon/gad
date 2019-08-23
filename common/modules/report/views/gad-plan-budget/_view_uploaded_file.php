601b033dba39a4f19618e6881cdf3bae3ed4161a
Mark Bon <mdbon@dilg.gov.ph>
2019-08-23 14:40:25 +0800

    try resolving the urlfrontendmanager issue

--- common/modules/report/views/gad-plan-budget/_view_uploaded_file.php
+++ common/modules/report/views/gad-plan-budget/_view_uploaded_file.php
@@ -8,12 +8,12 @@ use yii\helpers\Url;
 <?php 
 if($extension == "jpeg" || $extension == "jpg" || $extension == "png")
 {
-	echo Html::img(\Yii::$app->urlManagerFrontend->baseUrl."/uploads/file_attached/".$file, ['alt'=>'images','class'=>'img-responsive']);
+	echo Html::img(Url::base()."/uploads/file_attached/".$file, ['alt'=>'images','class'=>'img-responsive']);
 }
 else
 {
 ?>
-	<iframe src="<?= \Yii::$app->urlManagerFrontend->baseUrl."/uploads/file_attached/".$file ?>" style="width: 100%; height: 650px;"></iframe>
+	<iframe src="<?= Url::base()."/uploads/file_attached/".$file ?>" style="width: 100%; height: 650px;"></iframe>
 <?php 
 }
 ?>
