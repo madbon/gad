a23ffb35bb822ba9d20a0644086011937ef72664
Mark Bon <mdbon@dilg.gov.ph>
2019-08-23 15:13:22 +0800

    fullscreen of pdf and image attachments

--- common/modules/report/views/gad-plan-budget/_view_uploaded_file.php
+++ common/modules/report/views/gad-plan-budget/_view_uploaded_file.php
@@ -5,16 +5,27 @@ use yii\helpers\Url;
 ?>
  <!-- $file = "/hris/backend/web/file/file/; -->
 <br/>
+
 <?php 
+echo  Html::a('<i class="glyphicon glyphicon-fullscreen"></i> Full Screen', 
+     Url::base()."/uploads/file_attached/".$file, 
+     [
+         'class' => 'pull-right',
+         'style' => 'font-size:15px; font-weight:bold;'
+         // 'target' => '_blank'
+]);
+
 if($extension == "jpeg" || $extension == "jpg" || $extension == "png")
 {
 	echo Html::img(Url::base()."/uploads/file_attached/".$file, ['alt'=>'images','class'=>'img-responsive']);
+
 }
 else
 {
 ?>
 	<iframe src="<?= Url::base()."/uploads/file_attached/".$file ?>" style="width: 100%; height: 650px;"></iframe>
 <?php 
+
 }
 ?>
 
